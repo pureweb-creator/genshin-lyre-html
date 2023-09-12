deab8d59601f3f5ae6fa221fa83cc03d052608f5
Roman Semenikhin <roman.semenikhin99@gmail.com>
2023-09-12 16:42:46 +0300

    initital commit

---
+++ inc/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en_GB">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= $data->title ?></title>
+
+	<link rel="stylesheet" href="<?= $data->home_url ?>/static/css/remodal.css">
+	<link rel="stylesheet" href="<?= $data->home_url ?>/static/css/remodal-default-theme.css">
+    <link rel="stylesheet" href="<?= $data->home_url ?>/static/css/main.css">
+</head>
+<body>
